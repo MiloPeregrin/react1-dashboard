d7b1ab11753dca66adb0a2da8dc1b2d88ce1a5ac
MiloPeregrin <milanperegrin@gmail.com>
2022-05-12 10:49:04 +0200

    bugfix

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -38,6 +38,7 @@ const Form = ({ mode, selectedTask }: IForm) => {
       if (initial) {
         setInputState(initial);
       }
+    } else {
       setInputState(selectedTask);
     }
   }, []);
